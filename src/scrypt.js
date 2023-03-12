1ace15a48773c21485957e7a085947bfbdec503d
Gabriel Sousa <gabriel2001@live.com.pt>
2023-03-12 13:17:23 +0000

    Update code

--- src/scrypt.js
+++ src/scrypt.js
@@ -25,14 +25,15 @@ const images = [
   "images/MPS_Donkey_Kong_Artwork.webp",
   "images/MPSS_Peach.webp",
 ];
+const tabs = [firstTab, secondTab, thirdTab];
+const tabContents = [firstTabs, secondTabs, thirdTabs];
 let counter = 0;
 
-  if (localRead) {
-    switchLanguage(localRead);
-  }else{
-    localStorage.setItem("language", "pt")
-  }
-  
+if (localRead) {
+  switchLanguage(localRead);
+} else {
+  localStorage.setItem("language", "pt");
+}
 
 seeLateral.addEventListener("click", () => {
   lateralDiv.style.display = "block";
@@ -44,25 +45,29 @@ seeLateral.addEventListener("click", () => {
   });
 });
 
-firstTab.addEventListener("click", () => {
-  firstTabs.style.display = "flex";
-  secondTabs.style.display = "none";
-  thirdTabs.style.display = "none";
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content, i) => {
+      content.style.display = i === index ? "flex" : "none";
+    });
+  });
 });
 
-secondTab.addEventListener("click", () => {
-  firstTabs.style.display = "none";
-  secondTabs.style.display = "flex";
-  thirdTabs.style.display = "none";
-});
+next.addEventListener("click", () => changeImage(1));
+before.addEventListener("click", () => changeImage(-1));
 
-thirdTab.addEventListener("click", () => {
-  firstTabs.style.display = "none";
-  secondTabs.style.display = "none";
-  thirdTabs.style.display = "flex";
-});
+function changeImage(delta) {
+  counter += delta;
+  if (counter < 0) {
+    counter = images.length - 1;
+  } else if (counter >= images.length) {
+    counter = 0;
+  }
+  changeImage.src = images[counter];
+}
 
-next.addEventListener("click", () => {
+
+/*next.addEventListener("click", () => {
   if (counter < images.length - 1) {
     counter++;
     changeImage.src = images[counter];
@@ -79,7 +84,7 @@ before.addEventListener("click", () => {
     counter = images.length - 1;
     changeImage.src = images[counter];
   }
-});
+});*/
 
 closet.addEventListener("mouseenter", () => {
   closetMario.style.transform = "rotate(60deg) translate(30%)";
@@ -87,8 +92,6 @@ closet.addEventListener("mouseenter", () => {
     closetMario.style.transform = "rotate(0deg)";
   });
 });
-
-
 
 closet.addEventListener("click", () => {
   modal.style.display = "flex";
@@ -109,19 +112,11 @@ language.addEventListener("change", () => {
 });
 
 function switchLanguage(lang) {
-  const MarioEn = "this doesn't really work in english.";
-  const MarioPt = "Mario? Aquele que te comeu atras do armario.";
-  const MarioCn = "读白 不要忘记 不要错过 记得去电影院看《速度与激情9 因为非常好电影 动作非常好 差不多一样冰淇淋 再见";
-  const langQuote = document.getElementById("langQuote");
-
-  if (lang === "en") {
-    langQuote.innerHTML = MarioEn;
-  } else if (lang === "pt") {
-    langQuote.innerHTML = MarioPt;
-  } else if (lang === "cn") {
-    langQuote.innerHTML = MarioCn;
+  const trans = {
+    en: "this doesn't really work in english.",
+    pt: "Mario? Aquele que te comeu atras do armario.",
+    cn: "读白 不要忘记 不要错过 记得去电影院看《速度与激情9 因为非常好电影 动作非常好 差不多一样冰淇淋 再见",
   }
+  const langQuote = document.getElementById("langQuote");
+  langQuote.innerHTML = trans[lang];
 }
-
-
-
